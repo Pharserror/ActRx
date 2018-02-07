fb01371e4b3b914170d2ad7ea1c868129d5d3900
Adam Lind <alind@teladoc.com>
2018-02-07 11:41:58 -0500

    Version 0.2.0. Forgot to make this a umd library.

--- dist/actrx.js
+++ dist/actrx.js
@@ -1,4 +1,14 @@
-/******/ (function(modules) { // webpackBootstrap
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["ActRx"] = factory();
+	else
+		root["ActRx"] = factory();
+})(typeof self !== 'undefined' ? self : this, function() {
+return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -137,3 +147,4 @@ exports.default = SETTINGS;
 
 /***/ })
 /******/ ]);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   context: __dirname, // we just want to use $cwd
-  // entry is the file that renders our React app.
   entry: {
     actrx: [
       './src/plug.js'
@@ -10,20 +9,19 @@ module.exports = {
     path: __dirname + '/dist',
     // filename is the name of the output file that will be compiled by Webpack
     filename: '[name].js',
-    publicPath: '/'
+    publicPath: '/',
+    library: 'ActRx',
+    libraryTarget: 'umd'
   },
   module: {
     // our loaders are our transpilers and interpreters such as Babel
     loaders: [{
-      test: /\.js$/, // we tell babel to look for js and jsx files
+      test: /\.js$/,
       exclude: /node_modules/, // we expect our node modules to already be transpiled
       loader: 'babel-loader',
       query: {
-        presets: ['es2015', 'stage-0'] // we need to use this preset so that Babel doesn't choke on JSX syntax
+        presets: ['es2015', 'stage-0']
       }
-    }, {
-      test: /\.s?css$/,
-      loaders: ['style', 'css', 'sass']
     }]
   },
   resolve: {
